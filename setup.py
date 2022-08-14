c34e6f7cfbd36879f1db33bd3d046db1abae45cd
LePhobix_ <40542349+kobalt66@users.noreply.github.com>
2022-08-14 13:41:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "BeeNary",
-    version = "1.2",
+    version = "1.2.1",
     description = "This package contains the interpreter for the toy language BeeNary.",
     author = "LePhobix",
     author_email = "code4beenary.gmail.com",
